da66c181f628254ee8b407a1281492e3d3221b87
254guru <otienooluda@gmail.com>
2024-03-05 10:02:07 +0000

    add documentation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,10 +5,14 @@ simple algorithm to generate Pascal's triangle
 
 
 def pascal_triangle(n):
+    """
+    generate Pascal's triangle upto the nth row
+    """
     if n <= 0:
         return []
 
     triangle = []
+
     for i in range(n):
         row = [1] * (i + 1)
         if i > 1:
