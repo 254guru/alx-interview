9be3d5f047711b35ed6c0aa03a4fc114e4c6543f
254guru <otienooluda@gmail.com>
2024-03-05 09:49:24 +0000

    pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+simple algorithm to generate Pascal's triangle
+"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        if i > 1:
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    return triangle
+
+
+# Test the function with the provided main script
+if __name__ == "__main__":
+    triangle = pascal_triangle(5)
+    for row in triangle:
+        print(row)
