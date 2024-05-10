4126c80421ff8e67d82b128a7034765d2ece13ba
254guru <otienooluda@gmail.com>
2024-05-10 13:07:43 +0000

    change approach to the function

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,22 +6,15 @@ of the island described in grid
 
 
 def island_perimeter(grid):
+    """declare variables:
+        perimeter: set value to 0"""
     perimeter = 0
-
     for i in range(len(grid)):
-        for j in range(len(grid[0])):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 perimeter += 4
-                # Each land cell contributes 4 to the perimeter
-
-                # Check left cell
-                if j > 0 and grid[i][j - 1] == 1:
+                if i > 0 and grid[i-1][j] == 1:
                     perimeter -= 2
-                    # Reduce perimeter by 2 if left cell is land
-
-                # Check top cell
-                if i > 0 and grid[i - 1][j] == 1:
+                if j > 0 and grid[i][j-1] == 1:
                     perimeter -= 2
-                    # Reduce perimeter by 2 if top cell is land
-
     return perimeter
